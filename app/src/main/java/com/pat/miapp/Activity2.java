90843da088a8ecbdf8a3e9326461938ddb82e837
Patrick <p.segovia10@alumnos.santotomas.cl>
2024-09-08 19:04:15 -0300

    Commit inicial

---
+++ app/src/main/java/com/pat/miapp/Activity2.java
@@ -0,0 +1,11 @@
+package com.pat.miapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Activity2 extends AppCompatActivity {
+
+
+}
---
+++ app/src/main/java/com/pat/miapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.pat.miapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etDatos;
+    private Button btnEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etDatos = findViewById(R.id.etDatos);
+        btnEnviar = findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String datos = etDatos.getText().toString();
+                enviarDatos(datos);
+            }
+        });
+    }
+//
+
+}
