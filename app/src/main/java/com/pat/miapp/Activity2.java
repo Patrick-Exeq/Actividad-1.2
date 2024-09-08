7405cea552f30504b99bf05d7c681011f4a6ff22
Patrick <p.segovia10@alumnos.santotomas.cl>
2024-09-08 19:08:30 -0300

    Activity2 completa

--- app/src/main/java/com/pat/miapp/Activity2.java
+++ app/src/main/java/com/pat/miapp/Activity2.java
@@ -7,5 +7,24 @@ import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
 
+    private TextView tvResultado;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        tvResultado = findViewById(R.id.tvResultado);
+
+        // Obtener los datos del Intent
+        String datosRecibidos = getIntent().getStringExtra("clave_datos");
+
+        if (datosRecibidos != null) {
+            // Mostrar los datos en el TextView
+            tvResultado.setText(datosRecibidos);
+
+            // Mostrar los datos en un Toast
+            Toast.makeText(this, "Datos recibidos: " + datosRecibidos, Toast.LENGTH_LONG).show();
+        }
+    }
 }
