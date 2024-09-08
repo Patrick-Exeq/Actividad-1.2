dd00e7614a3de321e1e7f796d09c2ee3bbd5140c
Patrick <p.segovia10@alumnos.santotomas.cl>
2024-09-08 19:07:41 -0300

    Método en para enviar datos

--- app/src/main/java/com/pat/miapp/MainActivity.java
+++ app/src/main/java/com/pat/miapp/MainActivity.java
@@ -28,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-//
+    // Método para enviar los datos a la segunda Activity
+    private void enviarDatos(String datos) {
+        Intent intent = new Intent(MainActivity.this, Activity2.class);
+        intent.putExtra("clave_datos", datos);
+        startActivity(intent);
+    }
 
 }
