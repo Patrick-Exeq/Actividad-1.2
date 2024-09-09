adbf5bc4497cf4e456ff53b4b307f74a0eefca1d
Patrick <p.segovia10@alumnos.santotomas.cl>
2024-09-09 03:48:15 -0300

    Commit nombres

--- app/src/main/java/com/pat/miapp/MainActivity.java
+++ app/src/main/java/com/pat/miapp/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+// Autores: Patrick Segovia - Rick Gonzales
 public class MainActivity extends AppCompatActivity {
 
     private EditText etDatos;
